12be47393c2862dd8128c74784434632bddd4f74
Matheus Kraisfeld <matheuskraisfeld@gmail.com>
2021-10-03 19:38:38 -0300

    Update write-a-function.py

--- write-a-function.py
+++ write-a-function.py
@@ -1,11 +1,2 @@
-def is_leap(year):
-    leap = False
-    
-    if (year % 4) == 0:
-        if (year % 100) == 0:
-            if (year % 400) == 0:
-                leaf = True
-        else:
-            leaf = True
-    
-    return leap
+def is_leap(year):    
+    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
